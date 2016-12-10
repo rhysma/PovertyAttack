b9192ee03acd410222268b45939d5995c2dde2d0
rhysma <rhysma@gmail.com>
2016-12-10 14:05:02 -0600

    Provider detail is ready

--- PoveryAttack/ProviderDetailActivity.cs
+++ PoveryAttack/ProviderDetailActivity.cs
@@ -42,18 +42,68 @@ namespace PoveryAttack
 
             //get the specific record for the recordID
             var record = from i in items
-                                       where i.RESOURCEID == resourceID
-                                       select i;
+                         where i.RESOURCEID == resourceID
+                         select i;
 
-            TextView providerName = FindViewById<TextView>(Resource.Id.providerNameBox);
-            providerName.Text = record.RESOURCENAME;
-            TextView address1 = FindViewById<TextView>(Resource.Id.address1Box);
-            address1.Text = record.ADDRESS1;
-            TextView address2 = FindViewById<TextView>(Resource.Id.address2Box);
-            address2.Text = record.ADDRESS2;
-            string cityStateZip = record.CITY + ", " + record.STATE + " " + record.ZIP;
-            TextView city = FindViewById<TextView>(Resource.Id.cityBox);
-            city.Text = cityStateZip;
+            foreach(var r in record)
+            {
+                TextView providerName = FindViewById<TextView>(Resource.Id.providerNameBox);
+                providerName.Text = r.RESOURCENAME;
+                TextView address1 = FindViewById<TextView>(Resource.Id.address1Box);
+                address1.Text = r.ADDRESS1;
+                TextView address2 = FindViewById<TextView>(Resource.Id.address2Box);
+                address2.Text = r.ADDRESS2;
+                string cityStateZip = r.CITY + ", " + r.STATE + " " + r.ZIP;
+                TextView city = FindViewById<TextView>(Resource.Id.cityBox);
+                city.Text = cityStateZip;
+                TextView county = FindViewById<TextView>(Resource.Id.countyBox);
+                county.Text = r.COUNTY + " County";
+                TextView phone = FindViewById<TextView>(Resource.Id.phoneBox);
+                phone.Text = r.PHONE;
+                TextView services = FindViewById<TextView>(Resource.Id.servicesBox);
+                services.Text = r.SERVICES;
+                ImageButton fbButton = FindViewById<ImageButton>(Resource.Id.fbButton);
+                if(r.FACEBOOKURL == "")
+                {
+                    fbButton.Enabled = false;
+                }
+                fbButton.Click += delegate
+                {
+                    var uri = Android.Net.Uri.Parse(r.FACEBOOKURL);
+                    var intent = new Intent(Intent.ActionView, uri);
+                    StartActivity(intent);
+                };
+
+                ImageButton websiteButton = FindViewById<ImageButton>(Resource.Id.websiteButton);
+                if (r.WEBPAGE == "")
+                {
+                    fbButton.Enabled = false;
+                }
+                websiteButton.Click += delegate
+                {
+                    var uri = Android.Net.Uri.Parse(r.WEBPAGE);
+                    var intent = new Intent(Intent.ActionView, uri);
+                    StartActivity(intent);
+                };
+
+                ImageButton emailButton = FindViewById<ImageButton>(Resource.Id.emailButton);
+                if (r.EMAIL == "")
+                {
+                    fbButton.Enabled = false;
+                }
+                emailButton.Click += delegate
+                {
+                    var email = new Intent(Android.Content.Intent.ActionSend);
+                    email.PutExtra(Android.Content.Intent.ExtraCc, new string[] { r.EMAIL });
+
+                    email.PutExtra(Android.Content.Intent.ExtraSubject, "Email Message from App");
+                    email.SetType("message/rfc822");
+                    StartActivity(email);
+                };
+
+            }
+
+            
 
 
         }
